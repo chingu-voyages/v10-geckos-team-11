bbe50d711eb07024ee19b3cf6022adac531b7ffc
Matthew Kim <minwook.kim87@gmail.com>
2019-08-07 23:50:27 +1000

    Moved file, added to src.index.js

--- stories/Border.stories.js
+++ stories/Border.stories.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import Border from '../src/Border/Border'
+import Border from '../src/Border/'
 
 storiesOf('Border', module)
   .add('shapes', () => {
